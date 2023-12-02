3d451b1d29ba6c26aa49409cac81531027f45ba8
David Furlong <dvfurlong@googlemail.com>
2023-12-02 23:12:25 +0100

    fix: homepage

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,12 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
+  const [fid, setFid] = useState("");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Try going to /2022/[yourfid]
-      <Link href={`/2022/1214`}>@df</Link>
-      <Link href={`/2022/3`}>@dwr</Link>
+      <p>Farcaster wrapped 2022</p>
+      <input
+        value={fid}
+        onChange={(e) => setFid(e.currentTarget.value)}
+        placeholder="Put your fid here"
+      />
+      <Link href={`/2022/${fid}`}>Go</Link>
+      <p>
+        Example: <Link href={`/2022/1214`}>@df</Link>
+      </p>
     </main>
   );
 }
